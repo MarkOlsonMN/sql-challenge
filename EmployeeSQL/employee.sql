cf2f2397fc8807a7d3ecdb5080109c3a808c35f3
MarkOlsonMN <mark.olson.linkedin@gmail.com>
2024-06-04 00:39:16 -0500

    assignment release candidate RC1

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -1,3 +1,6 @@
+-- ####################
+-- # DATA ENGINEERING #
+-- ####################
 
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
@@ -5,7 +8,6 @@ DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS titles;
-
 
 CREATE TABLE titles (
     title_id VARCHAR(10) PRIMARY KEY,
@@ -28,14 +30,11 @@ CREATE TABLE employees (
     FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
-
 CREATE TABLE salaries (
-    emp_no INT,
+    emp_no INT PRIMARY KEY,
     salary INT NOT NULL,
-    PRIMARY KEY (emp_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
-
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR(10) NOT NULL,
@@ -56,12 +55,31 @@ CREATE TABLE dept_emp (
 
 
 
+-- ###################################################
+-- # NOTE: At this point I imported each CSV file    #
+-- #       into its corresponding SQL database table #
+-- ###################################################
+--
+-- ... imported titles
+-- ... imported departments
+-- ... imported employees
+-- ... imported salaries
+-- ... imported dept_manager
+-- ... imported dept_emp
+
+
+
+
+-- #################
+-- # DATA ANALYSIS #
+-- #################
+
 -- ##########################################################################################
 -- # 1.) List the employee number, last name, first name, sex, and salary of each employee. #
 -- ##########################################################################################
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
-LEFT JOIN salaries AS s ON (e.emp_no = s.emp_no);
+JOIN salaries AS s ON (e.emp_no = s.emp_no);
 
 -- ###############################################################################################
 -- # 2.) List the first name, last name, and hire date for the employees who were hired in 1986. #
@@ -76,8 +94,8 @@ WHERE EXTRACT(YEAR FROM e.hire_date) = 1986;
 -- #######################################################################################
 SELECT dm.dept_no, dp.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM dept_manager AS dm
-LEFT JOIN departments AS dp ON (dm.dept_no = dp.dept_no)
-LEFT JOIN employees AS e ON (dm.emp_no = e.emp_no);
+JOIN departments AS dp ON (dm.dept_no = dp.dept_no)
+JOIN employees AS e ON (dm.emp_no = e.emp_no);
 
 -- ###############################################################################
 -- # 4.) List the department number for each employee along with that employee’s #
@@ -85,8 +103,8 @@ LEFT JOIN employees AS e ON (dm.emp_no = e.emp_no);
 -- ###############################################################################
 SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, dp.dept_name
 FROM dept_emp AS de
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no);
+JOIN departments AS dp ON (de.dept_no = dp.dept_no)
+JOIN employees AS e ON (de.emp_no = e.emp_no);
 
 -- ############################################################
 -- # 5.) List first name, last name, and sex of each employee #
@@ -103,7 +121,7 @@ WHERE (e.first_name = 'Hercules') AND (e.last_name LIKE 'B%');
 -- ###################################################################
 SELECT e.emp_no, e.last_name, e.first_name
 FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
+JOIN employees AS e ON (de.emp_no = e.emp_no)
 WHERE de.dept_no = (
 	SELECT dp.dept_no
 	FROM departments AS dp	
@@ -117,8 +135,8 @@ WHERE de.dept_no = (
 -- ####################################################################################
 SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
 FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
+JOIN employees AS e ON (de.emp_no = e.emp_no)
+JOIN departments AS dp ON (de.dept_no = dp.dept_no)
 WHERE de.dept_no IN (
 	SELECT dp.dept_no
 	FROM departments AS dp	
@@ -129,148 +147,7 @@ WHERE de.dept_no IN (
 -- # 8.) List the frequency counts, in descending order, of all the employee last names #
 -- #     (that is, how many employees share each last name).                            #
 -- ######################################################################################
-SELECT e.last_name, COUNT(*) AS total_count
+SELECT e.last_name, COUNT(*) AS frequency_count
 FROM employees AS e
 GROUP BY e.last_name
-ORDER BY total_count DESC;
-
-	
-
-
-
-
-
-
-
--- NOTES
-
-
-
-SELECT DISTINCT e.last_name
-FROM employees AS e
-ORDER BY e.last_name DESC;
-
-SELECT COUNT(*) AS total_count
-FROM employees AS e
-WHERE e.last_name ='Sadowsky';
-
-
-
-
--- NOTES
-
-
---
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = 'd007';
--- 52245
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = 'd005';
--- 85707
---
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Sales'
-	LIMIT 1
-);
--- 52245
---
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Development'
-	LIMIT 1
-);
--- 85707
---
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Sales'
-	LIMIT 1
-)
-UNION
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Development'
-	LIMIT 1
-);
--- 52245
--- 85707
---
---
-SELECT COUNT(*) AS total_count
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no IN (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name in ('Sales','Development')
-);
--- 137952
---
---
---
-SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no IN (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name in ('Sales','Development')
-);
---
---
---
-SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Sales'
-	LIMIT 1
-)
-UNION
-SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
-FROM dept_emp AS de
-LEFT JOIN employees AS e ON (de.emp_no = e.emp_no)
-LEFT JOIN departments AS dp ON (de.dept_no = dp.dept_no)
-WHERE de.dept_no = (
-	SELECT dp.dept_no
-	FROM departments AS dp	
-	WHERE dp.dept_name = 'Development'
-	LIMIT 1
-);
+ORDER BY frequency_count DESC;
